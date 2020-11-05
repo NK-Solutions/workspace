3074a8031d45d7c0d2bccd34ad313acdb0cc271f
Cryptec <neupi92@web.de>
2020-11-05 13:38:05 +0100

    Updates

---
+++ precache-manifest.8fde75406e6af561f10c99f64a10b10c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cfd3e2d0a74e193326d91e205db8fa8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85ca5a7330ed89fecb2e",
+    "url": "/static/css/main.0be374db.chunk.css"
+  },
+  {
+    "revision": "0ee237336fc90201f893",
+    "url": "/static/js/2.b2e7c252.chunk.js"
+  },
+  {
+    "revision": "90d102df77735e42f7069ff459bc4a16",
+    "url": "/static/js/2.b2e7c252.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85ca5a7330ed89fecb2e",
+    "url": "/static/js/main.52a8a1b8.chunk.js"
+  },
+  {
+    "revision": "35b79093e77ed2ff88be",
+    "url": "/static/js/runtime-main.050c7c29.js"
+  },
+  {
+    "revision": "0890fdc12a745029bb417befc8223c5d",
+    "url": "/static/media/NK.0890fdc1.svg"
+  }
+]);
